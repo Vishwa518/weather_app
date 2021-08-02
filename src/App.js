1a9227f2311d44e2e0abe94c1207f13db42594a0
Vishwa <vishwanathsomavarad@gmail.com>
2021-08-02 20:58:03 +0530

    --new changes

--- src/App.js
+++ src/App.js
@@ -1,100 +1,48 @@
-import './App.css';
 import React, { useEffect, useState } from "react";
-import { Dimmer, Loader } from 'semantic-ui-react';
-import Weather from './components/weather';
-import Forecast from './components/forecast';
-export default function App() {
-  
+import { Dimmer, Loader } from "semantic-ui-react";
+import Weather from "./components/weather";
+
+function App() {
   const [lat, setLat] = useState([]);
   const [long, setLong] = useState([]);
-  const [weatherData, setWeatherData] = useState([]);
-  const [forecast, setForecast] = useState([]);
-  const [error, setError] = useState(null);
+  const [data, setData] = useState([]);
+  const [cityName, setCityName] = useState("Yattinahalli");
 
   useEffect(() => {
-      navigator.geolocation.getCurrentPosition(function(position) {
+    const fetchData = async () => {
+      navigator.geolocation.getCurrentPosition(function (position) {
         setLat(position.coords.latitude);
         setLong(position.coords.longitude);
-      });
-    
-      getWeather(lat, long)
-      .then(weather => {
-        setWeatherData(weather);
-        setError(null);
-      })
-      .catch(err => {
-        setError(err.message);
+
+        var GEOCODING = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=' + position.coords.latitude + '%2C' + position.coords.longitude + '&language=en';
+      
+           console.log('GEOCODING',GEOCODING);
       });
 
-      getForecast(lat, long)
-        .then(data => {
-          setForecast(data);
-          setError(null);
-        })
-        .catch(err => {
-          setError(err.message);
+      await fetch(
+        `${process.env.REACT_APP_API_URL}/weather/?q=${cityName}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`
+      )
+        .then((res) => res.json())
+        .then((result) => {
+          setData(result);
+          console.log("result", result);
         });
-
-  }, [lat,long,error])
-
-  function handleResponse(response) {
-    if (response.ok) {
-      return response.json();
-    } else {
-      throw new Error("Please Enable your Location in your browser!");
-    }
-  }
-
-  function getWeather(lat, long) {
-    return fetch(
-      `${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`
-    )
-      .then(res => handleResponse(res))
-      .then(weather => {
-        if (Object.entries(weather).length) {
-          const mappedData = mapDataToWeatherInterface(weather);
-          return mappedData;
-        }
-      });
-  }
-  
-  function getForecast(lat, long) {
-    return fetch(
-      `${process.env.REACT_APP_API_URL}/forecast/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`
-    )
-      .then(res => handleResponse(res))
-      .then(forecastData => {
-        if (Object.entries(forecastData).length) {
-          return forecastData.list
-            .filter(forecast => forecast.dt_txt.match(/09:00:00/))
-            .map(mapDataToWeatherInterface);
-        }
-      });
-  }
-
-  function mapDataToWeatherInterface(data) {
-    const mapped = {
-      date: data.dt * 1000, // convert from seconds to milliseconds
-      description: data.weather[0].main,
-      temperature: Math.round(data.main.temp),
     };
-  
-    // Add extra properties for the five day forecast: dt_txt, icon, min, max
-    if (data.dt_txt) {
-      mapped.dt_txt = data.dt_txt;
-    }
-  
-    return mapped;
-  }
-  
+    fetchData();
+  }, [lat, long]);
   return (
     <div className="App">
-      {(typeof weatherData.main != 'undefined') ? (
-        <div>
-          <Weather weatherData={weatherData}/>
-          <Forecast forecast={forecast} weatherData={weatherData}/>
-        </div>
-      ): (
+      <React.Fragment>
+        <input
+          type="text"
+          placeholder="search"
+          value={cityName}
+          onChange={(e) => setCityName(e)}
+        />
+      </React.Fragment>
+      {typeof data.main != "undefined" ? (
+        <Weather weatherData={data} />
+      ) : (
         <div>
           <Dimmer active>
             <Loader>Loading..</Loader>
@@ -104,3 +52,5 @@ export default function App() {
     </div>
   );
 }
+
+export default App;
